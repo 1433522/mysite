2d787011a6ab17b8f675820024ccbcb5662e2431
zzx <me@zhangzhixiong.com>
2021-01-01 22:34:01 +0800

    v1.0.1

--- diary/admin.py
+++ diary/admin.py
@@ -9,5 +9,6 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('id','title','date','category')
+    search_fields = ('title','body')
     ordering = ('-date',)
     list_display_links = ('id', 'title')
